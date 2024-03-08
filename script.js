e540892f90d478ae5cf7a3ee6d3d6f2cb6f7a947
kannanATexcelforte <kannan@excelforte.com>
2024-03-08 07:42:12 +0530

    08.03.2024
    basic array operations(methods) completed

--- script.js
+++ script.js
@@ -110,3 +110,37 @@ const percentages = [
     percentageOfWorld1(populations[3])
 ];
 console.log(percentages);
+
+// basic array operations(methods) ////////////////////////////
+
+// Create an array containing all the neighbouring countries of a country of your choice.
+// Choose a country which has at least 2 or 3 neighbours. Store the array into a variable
+// called neighbours.
+
+let country = 'India';
+let neighbours = ["Afghanistan", "Bangladesh", "Bhutan", "China", "Maldives", "Myanmar", "Nepal", "Pakistan", "Sri Lanka"];
+
+// At some point, a new country called 'Utopia' is created in the neighbourhood of your selected
+// country, so add it to the end of the neighbours array.
+
+neighbours.push('Utopia');
+console.log(neighbours);
+
+// Unfortunately, after some time the new country is dissolved, so remove it from the end of the array.
+
+neighbours.pop('Utopia');
+console.log(neighbours);
+
+// If the neighbours array does not include the country 'Germany',
+// log to the console: 'Probably not a central european country :D'.
+
+if (!neighbours.includes('Germany')) {
+    console.log(`Probably not a central european country :D`);
+};
+
+// Change the name of one of your neighbouring countries. To do that, find the index of the country
+// in the neighbours array, and then use that index to change the array at that index position.
+// For example, you can search for 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+neighbours[neighbours.indexOf('China')] = 'Republic of China';
+console.log(neighbours);
