e3f3bd99b9e8a103c3c1c46460107b87fe8fb2d3
kannanATexcelforte <kannan@excelforte.com>
2024-03-08 11:08:57 +0530

    08.03.2024
    dot vs bracket notation has completed

--- script.js
+++ script.js
@@ -144,3 +144,31 @@ if (!neighbours.includes('Germany')) {
 
 neighbours[neighbours.indexOf('China')] = 'Republic of China';
 console.log(neighbours);
+
+// introduction to objects ///////////////////////////
+
+// Create an object called myCountry for a country of your choice, containing properties country,
+// capital, language, population and neighbours (an array like we used in previous assignments).
+
+const myCountry = {
+    country: 'India',
+    capital: 'New Delhi',
+    language: 'Hindi and Other languages',
+    population: 1407.6,
+    neighbours: ["Afghanistan", "Bangladesh", "Bhutan", "China", "Maldives", "Myanmar", "Nepal", "Pakistan", "Sri Lanka"]
+}
+
+// dot vs bracket notation ///////////////////////////////
+
+// Using the object from the previous assignment, log a string like this to the console:
+// 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki'.
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
+
+// Increase the country's population by two million using dot notation, and then decrease it by two million using bracket notation.
+
+myCountry.population += 2;
+console.log(myCountry.population);
+
+myCountry['population'] -= 2;
+console.log(myCountry.population);
