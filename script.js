d29eb81ea3c3265d0c564eb58a0356bc27a48e84
kskannandotwin <kskannan.win@gmail.com>
2024-03-09 05:32:18 +0530

    09.03.2024
    destructuring arrays completed

--- script.js
+++ script.js
@@ -1,112 +1,279 @@
 "use strict";
 
-// functions ////////////////////////
+// data structures, modern operators and strings ///////////////////////
 
-// Write a function called describeCountry which takes three parameters:
-// country, population and capitalCity. Based on this input, the function
-// returns a string with this format: 'Finland has 6 million people and
-// its capital city is Helsinki'.
+// introduction
 
-// Call this function 3 times, with input data for 3 different countries.
-// Store the returned values in 3 different variables, and log them to the console.
+/*
+Below you can find data used in assignments. It's an array of books related
+to computer science, math and business. Each book is represented by an object.
+Missing properties, different data types for same properties or duplicates are
+intentional. Copy this data to your code editor, and give yourself some time
+to familiarize with the data before you start working on assignments.
+*/
 
-function describeCountry(country, population, capitalCity) {
-    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
-}
-
-const descFinland = describeCountry('Finland', 10, 'Helsinki');
-const descIndia = describeCountry('India', 1407.6, 'New Delhi');
-const descEngland = describeCountry('England', 56, 'London');
-console.log(descFinland);
-console.log(descIndia);
-console.log(descEngland);
-
-// function declarations and expressions ///////////////////////
-
-// The world population is 7900 million people. Create a function declaration
-// called percentageOfWorld1 which receives a population value, and returns
-// the percentage of the world population that the given population represents.
-// For example, China has 1441 million people, so it's about 18.2% of the world population.
-
-// To calculate the percentage, divide the given population value by 7900 and then multiply by 100.
-
-// Call percentageOfWorld1 for 3 populations of countries of your choice,
-// store the results into variables, and log them to the console.
-
-function percentageOfWorld1(population) {
-    return ((population / 7900) * 100).toFixed(2);
-}
-
-const finPercentage1 = percentageOfWorld1(10);
-const indPercentage1 = percentageOfWorld1(1407.6);
-const engPercentage1 = percentageOfWorld1(56);
-console.log(finPercentage1, indPercentage1, engPercentage1);
-
-// Create a function expression which does the exact same thing, called percentageOfWolrd2,
-// and also call it with 3 country populations (can be the same populations).
-
-const percentageOfWorld2 = function (population) {
-    return ((population / 7900) * 100).toFixed(2);
-};
-
-const finPercentage2 = percentageOfWorld2(10);
-const indPercentage2 = percentageOfWorld2(1407.6);
-const engPercentage2 = percentageOfWorld2(56);
-console.log(finPercentage2, indPercentage2, engPercentage2);
-
-// arrow functions ////////////////////////////
-
-// Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
-
-const percentageOfWorld3 = population => ((population / 7900) * 100).toFixed(2);
-
-const finPercentage3 = percentageOfWorld3(10);
-const indPercentage3 = percentageOfWorld3(1407.6);
-const engPercentage3 = percentageOfWorld3(56);
-console.log(finPercentage3, indPercentage3, engPercentage3);
-
-// functions calling other functions ///////////////////////////
-
-// Create a function called describePopulation. Use the function type you like the most.
-// This function takes in two arguments: country and population, and returns a strings like this:
-// 'China has 1441 million people, which is about 18.2% of the world'.
-
-// To calculate the percentage, describePopulation calls the percentageOfWorld1 you created earlier.
-
-// Call describePopulation with data for 3 countries of your choice.
-
-function describePopulation(country, population) {
-    const percentage = percentageOfWorld3(population);
-    const description = `${country} has ${population} million people, which is about ${percentage}% of the world`;
-    console.log(description);
-}
-
-describePopulation('Finland', 10);
-describePopulation('India', 1407.6);
-describePopulation('England', 56);
-
-// introduction to arrays /////////////////////////////
-
-// Create an array containing 4 population values of 4 countries of your choice.
-// You may use the values you have been using previously. Store this array into a
-// variable called populations.
-
-describePopulation('China', 1441);
-const populations = [10, 1407.6, 56, 1441];
-
-// Log to the console whether the array has 4 elements or not (true or false).
-const isFour = populations.length === 4;
-console.log(isFour);
-
-// Create an array called percentages containing the percentages of the world population
-// for these 4 population values. Use the function percentageOfWorld1 that you created earlier
-// to compute the 4 percentage values.
-
-const chiPercentage1 = percentageOfWorld1(1441);
-const percentages = [
-    percentageOfWorld1(populations[0]),
-    percentageOfWorld1(populations[1]),
-    percentageOfWorld1(populations[2]),
-    percentageOfWorld1(populations[3])
+const books = [
+  {
+    title: "Algorithms",
+    author: ["Robert Sedgewick", "Kevin Wayne"],
+    publisher: "Addison-Wesley Professional",
+    publicationDate: "2011-03-24",
+    edition: 4,
+    keywords: [
+      "computer science",
+      "programming",
+      "algorithms",
+      "data structures",
+      "java",
+      "math",
+      "software",
+      "engineering",
+    ],
+    pages: 976,
+    format: "hardcover",
+    ISBN: "9780321573513",
+    language: "English",
+    programmingLanguage: "Java",
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Structure and Interpretation of Computer Programs",
+    author: [
+      "Harold Abelson",
+      "Gerald Jay Sussman",
+      "Julie Sussman (Contributor)",
+    ],
+    publisher: "The MIT Press",
+    publicationDate: "2022-04-12",
+    edition: 2,
+    keywords: [
+      "computer science",
+      "programming",
+      "javascript",
+      "software",
+      "engineering",
+    ],
+    pages: 640,
+    format: "paperback",
+    ISBN: "9780262543231",
+    language: "English",
+    programmingLanguage: "JavaScript",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ["Randal E. Bryant", "David Richard O'Hallaron"],
+    publisher: "Prentice Hall",
+    publicationDate: "2002-01-01",
+    edition: 1,
+    keywords: [
+      "computer science",
+      "computer systems",
+      "programming",
+      "software",
+      "C",
+      "engineering",
+    ],
+    pages: 978,
+    format: "hardcover",
+    ISBN: "9780130340740",
+    language: "English",
+    programmingLanguage: "C",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Operating System Concepts",
+    author: ["Abraham Silberschatz", "Peter B. Galvin", "Greg Gagne"],
+    publisher: "John Wiley & Sons",
+    publicationDate: "2004-12-14",
+    edition: 10,
+    keywords: [
+      "computer science",
+      "operating systems",
+      "programming",
+      "software",
+      "C",
+      "Java",
+      "engineering",
+    ],
+    pages: 921,
+    format: "hardcover",
+    ISBN: "9780471694663",
+    language: "English",
+    programmingLanguage: "C, Java",
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: "Engineering Mathematics",
+    author: ["K.A. Stroud", "Dexter J. Booth"],
+    publisher: "Palgrave",
+    publicationDate: "2007-01-01",
+    edition: 14,
+    keywords: ["mathematics", "engineering"],
+    pages: 1288,
+    format: "paperback",
+    ISBN: "9781403942463",
+    language: "English",
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "The Personal MBA: Master the Art of Business",
+    author: "Josh Kaufman",
+    publisher: "Portfolio",
+    publicationDate: "2010-12-30",
+    keywords: ["business"],
+    pages: 416,
+    format: "hardcover",
+    ISBN: "9781591843528",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: "Crafting Interpreters",
+    author: "Robert Nystrom",
+    publisher: "Genever Benning",
+    publicationDate: "2021-07-28",
+    keywords: [
+      "computer science",
+      "compilers",
+      "engineering",
+      "interpreters",
+      "software",
+      "engineering",
+    ],
+    pages: 865,
+    format: "paperback",
+    ISBN: "9780990582939",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: "Deep Work: Rules for Focused Success in a Distracted World",
+    author: "Cal Newport",
+    publisher: "Grand Central Publishing",
+    publicationDate: "2016-01-05",
+    edition: 1,
+    keywords: ["work", "focus", "personal development", "business"],
+    pages: 296,
+    format: "hardcover",
+    ISBN: "9781455586691",
+    language: "English",
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
 ];
-console.log(percentages);
+
+// destructuring arrays ////////////////////////
+
+// 1.1
+
+// Destructure the books array into two variables called firstBook and secondBook.
+
+const [firstBook, secondBook] = books;
+console.log(firstBook, secondBook);
+
+// 1.2
+
+// Destructure the books array into a variable called thirdBook.
+// You must skip the first two books.
+
+const [, , thirdBook] = books;
+console.log(thirdBook);
+
+// 1.3
+
+// Below is the nested ratings array that contains two other arrays.
+// Destructure the nested ratings arrays into two variables called rating
+// and ratingsCount. In the result of your destructuring, the ratings
+// variable should store a number 4.19, and the ratingsCount variable
+// should store a number 144584.
+
+const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+
+const [[, rating], [, ratingsCount]] = ratings;
+console.log(rating, ratingsCount);
+
+// 1.4
+
+// Below is the ratingStars array. Destructure it into three variables called
+// fiveStarRatings, oneStarRatings and threeStarRatings. Assign the
+// threeStarRatings variable with a default value of 0.
+
+const ratingStars = [63405, 1808];
+
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
