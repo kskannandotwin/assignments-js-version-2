3df1f3ab1c4543dd5c9bcd08182b37549503bf55
kskannandotwin <kskannan.win@gmail.com>
2024-03-10 05:39:22 +0530

    10.03.2024
    destructuring objects completed except 2.6

--- script.js
+++ script.js
@@ -277,3 +277,53 @@ const ratingStars = [63405, 1808];
 
 const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
 console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+// destructuring objects /////////////////////////
+
+// 2.1
+
+// Destructure the first book object from the books array into variables called
+// title, author and ISBN.
+
+const { title, author, ISBN } = books[0];
+console.log(title, author, ISBN);
+
+// 2.2
+
+// Each book object has the keywords property. Destructure the first book object from the
+// books array into a variable called tags. The tags variable should be assigned with the
+// value of the keywords property.
+
+const { keywords: tags } = books[0];
+console.log(tags);
+
+// 2.3
+
+// The seventh book from the books array is missing the programmingLanguage property.
+// Destructure the seventh book object (books[6]) into variables called language and
+// programmingLanguage. Assign the programmingLanguage variable with a default value
+// of 'unknown'.
+
+const { language, programmingLanguage = 'unknown' } = books[6];
+console.log(language, programmingLanguage);
+
+// 2.4
+
+// Below are two variables called bookTitle and bookAuthor. Reassign them with the values
+// of the title and author properties of the first book object from the books array.
+
+const { title: bookTitle, author: bookAuthor } = books[0];
+console.log(bookTitle, bookAuthor);
+
+// 2.5
+
+// Each book object has a deeply nested rating property as illustrated below:
+
+// Destructure the first book object from the books array into a variable called bookRating.
+// In the result of your destructuring, the bookRating variable should be assigned with the
+// value of the book[0].thirdParty.goodreads.rating property.
+
+// Please do most of the work on the left side of the assignment operator: const ... = books[0];
+
+const { thirdParty: { goodreads: { rating: bookRating } } } = books[0];
+console.log(bookRating);
