9a7bafd8de246bfbd50d6e694a37440bcf4e67db
kannanATexcelforte <kannan@excelforte.com>
2024-03-11 16:02:13 +0530

    11.03.2024
    rest pattern and parameters has completed

--- script.js
+++ script.js
@@ -363,3 +363,36 @@ function spellWord(str) {
 }
 
 spellWord('Kannan');
+
+// rest pattern and parameters ///////////////////////////////////
+
+// 4.1
+
+// Destructure the keywords property (array) of the first book from the books array into variables
+// called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest
+// of the keywords should be assigned to the rest variable (it should be an array).
+
+const { mainKeyword, ...rest} = books[0].keywords;
+console.log(mainKeyword);
+console.log(rest);
+
+// 4.2
+
+// Destructure the second book from the books array into a variable called bookPublisher.
+// The bookPublisher variable should be assigned with the value of the publisher property
+// of the book object. Assign the rest of the properties to the restOfTheBook variable.
+
+const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+console.log(bookPublisher, restOfTheBook);
+
+// 4.3
+
+// Write a function called printBookAuthorsCount that has two parameters called title and authors.
+// The authors parameter should accept any number of arguments. This function should log to the
+// console a string formatted like that: "The book "${title}" has ${authors.length} authors".
+
+function printBookAuthorsCount(title, ...rest) {
+  console.log(`The book "${title}" has ${author.length} authors`);    
+}
+
+printBookAuthorsCount(title, rest);
