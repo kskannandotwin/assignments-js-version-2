0d1abb847cf67ece557939c72cb5aff65ccdaf97
kannanATexcelforte <kannan@excelforte.com>
2024-03-13 14:53:08 +0530

    13.03.2024
    short circuiting (&& and ||) has completed

--- script.js
+++ script.js
@@ -336,7 +336,7 @@ console.log(bookRating);
 
 // If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
 
-function printBookInfo(title, author, year='year unknown') {
+function printBookInfo(title, author, year = 'year unknown') {
   console.log(`${title} by ${author}, ${year}`);
 }
 
@@ -372,7 +372,7 @@ spellWord('Kannan');
 // called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest
 // of the keywords should be assigned to the rest variable (it should be an array).
 
-const { mainKeyword, ...rest} = books[0].keywords;
+const { mainKeyword, ...rest } = books[0].keywords;
 console.log(mainKeyword);
 console.log(rest);
 
@@ -392,7 +392,68 @@ console.log(bookPublisher, restOfTheBook);
 // console a string formatted like that: "The book "${title}" has ${authors.length} authors".
 
 function printBookAuthorsCount(title, ...rest) {
-  console.log(`The book "${title}" has ${author.length} authors`);    
+  console.log(`The book "${title}" has ${author.length} authors`);
 }
 
 printBookAuthorsCount(title, rest);
+
+// sort circuiting (&& and ||) ////////////////////////
+
+// 5.1
+
+// Some of the book objects have the programmingLanguage property, which specifies what programming
+// language is used in the book, for example
+/*
+{
+  title: 'Algorithms',
+  author: ['Robert Sedgewick', 'Kevin Wayne'],
+  ...
+  programmingLanguage: 'Java',     // <-- HERE
+}
+*/
+
+// Write a function called hasExamplesInJava that takes a book object from the books array as an argument.
+// This function should return true if the book uses Java, or a string 'no data available' if it uses other
+// language or no programming language at all.
+
+// Use short-circuiting
+
+function hasExamplesInJava(book) {
+  return book.programmingLanguage === 'Java' || 'No data available';
+}
+
+console.log(hasExamplesInJava(books[0]));
+
+// 5.2
+
+// Some of the book objects have the onlineContent property, which is either true or false.
+// Loop over the books array, and for the books that provide online content, log to the console
+// a string in this format: "${title}" provides online content. Use short-circuiting.
+
+// normal way
+
+function hasOnlineContent() {
+  for (let book of books) {
+    if (book.onlineContent) {
+      console.log(`${book.title} provides online content.`);
+    }
+  }
+}
+
+hasOnlineContent();
+
+// short circuiting way - 1
+
+function hasOnlineContent() {
+  for (let book of books) {
+    book.onlineContent && console.log(`${book.title} provides online content.`);
+  }
+}
+
+hasOnlineContent();
+
+// short circuiting way - tutorial way
+
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent && console.log(`"${books[i].title}" provides online content`);
+}
