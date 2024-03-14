738e8e45e217fddf781dbfefaf8c2efc36ffaee7
kskannandotwin <kskannan.win@gmail.com>
2024-03-14 20:55:53 +0530

    14.03.2024
    maps: iteration has completed

--- script.js
+++ script.js
@@ -788,3 +788,21 @@ console.log(bookMap.size);
 // to the console.
 
 if (bookMap.has("author")) console.log(`The author is known`);
+
+// maps: iteration //////////////////////////////
+
+// 14.1
+
+// Convert the first book object from the books array into a Map, and assign it
+// to a firstBookMap variable.
+
+const firstBookMap = new Map(Object.entries(books[0]));
+
+// 14.2
+
+// Use the for-of loop to iterate over firstBookMap, and log to the console keys that
+// have numbers as values.
+
+for (const [key, value] of firstBookMap) {
+  if (typeof value === 'number') console.log(key);
+}
