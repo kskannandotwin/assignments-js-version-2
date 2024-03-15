4e9b6803143f44226b77f4e8e4668a78f864dc6d
kannanATexcelforte <kannan@excelforte.com>
2024-03-15 11:39:47 +0530

    15.03.2024
    working with strings - part 3 has completed

--- script.js
+++ script.js
@@ -862,6 +862,15 @@ console.log(isContributor('Robert Sedgewick'));
 // You can be sure that the author's name always consists of two words separated by a space, and
 // possibly ends with "(Contributor)". The string may also contain trailing spaces.
 
+/*
+example:
+code:
+normalizeAuthorName('  JuliE sussMan (Contributor)')
+
+expected output:
+"Julie Sussman"
+*/
+
 function normalizeAuthorName(author) {
   author = author.trim();
   const firstName = author.slice(0, author.indexOf(' '));
@@ -916,5 +925,102 @@ function logBookTheme(title) {
     console.log('This book is about algorithms and data structures');
   } else if ((title.endsWith('system') || title.endsWith('systems')) && !title.includes('operating')) {
     console.log('This book is about some systems, but definitely not about operating systems');
+  }
+}
+
+// working with strings - part 3 //////////////////////////////
+
+// 17.1
+
+// Below is the bookCategories variable that stores a string of categories. Each category is separated
+// with a semicolon, for example, in a string "science;computing", 'science' and 'computing' are
+// separate categories.
+
+// Write a function called logBookCategories that takes a string of categories separated with semicolons,
+// and logs each category to the console (as separate strings).
+
+/*
+example:
+code:
+const bookCategories = 'science;computing;computer science;algorithms;business;operating systems;networking;electronics';
+logBookCategories(bookCategories);
+
+expected output:
+science
+computing
+computer science
+algorithms
+business
+operating systems
+networking
+electronics
+*/
+
+function logBookCategories(str) {
+  const categories = str.split(';');
+
+  for (let category of categories) {
+    console.log(category);
+  }
+}
+
+// 17.2
+
+// Now, the opposite. Each book from the books array has the keywords property.
+
+// Write a function called getKeywordsAsString that takes the books array as an argument, collects keywords
+// from each book, removes duplicates, and then joins them to create a single string where keywords
+// are separated by a semicolon.
+
+/*
+example:
+code:
+getKeywordsAsString(books);
+
+expected output:
+computer science;programming;algorithms;data structures;java;math;software;engineering;javascript;computer systems;C;operating systems;Java;mathematics;business;compilers;interpreters;work;focus;personal development
+*/
+
+function getKeywordsAsString(books) {
+  const keywords = [];
+
+  for (const book of books) {
+    keywords.push(...book.keywords);
+  }
+
+  const uniqueKeywords = [...new Set(keywords)];
+
+  return uniqueKeywords.join(';');
+}
+
+// 17.3
+
+// Below is the bookChapters array that contains inner arrays. Each inner array consists of a chapter's title,
+// and the number of a page, for example, in ['The Basics', 14], 'The Basics' is the chapter's title, and 14
+// is the number of a page.
+
+// Write a function called logBookChapters that takes an array of arrays (like bookChapters) as an argument,
+// and logs each chapter's name to the console together with the page number. The page number should be
+// separated from the chapter's name with underscores (take a look at the example below).
+
+// Use the padEnd method.
+
+/*
+example:
+code:
+const bookChapters = [['The Basics', 14], ['Sorting', 254], ['Searching', 372], ['Graphs', 526], ['Strings', 706]];
+logBookChapters(bookChapters);
+
+expected output:
+The Basics__________ 14
+Sorting_____________ 254
+Searching___________ 372
+Graphs______________ 526
+Strings_____________ 706
+*/
+
+function logBookChapters(chapters) {
+  for (const [chapter, pages] of chapters) {
+    console.log(chapter.padEnd(20, '_') + ' ' + pages);
   }
 }
